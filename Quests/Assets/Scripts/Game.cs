b14c686c40774f68c58f8678fef98707a2aff8c2
jerbyz <jer.crinson@gmail.com>
2018-01-23 10:37:02 -0500

    Editted Game.cs

--- Quests/Assets/Scripts/Game.cs
+++ Quests/Assets/Scripts/Game.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class Game : MonoBehaviour {
+private class Game {
+    //Player[] players;     //total players in the game
+    public int numPlayers;  //size of players Array
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+
+    //winlse is for finally of game, playerleft is for when AI or conclude game needs to be chosen
+    private enum state{menu, game, pause, playerLeft, winlose};
+    
+
+    //total turns taken so far (for progress)
+    int turns;
+
+    //setup for game
+	public void Game () {
+        /*
+         * Call script for asking the number of players playing
+         * Then, I/O from script is returned and numPLayers is set to it
+         */
+         turns=0;
+        //players = new player[numPlayers];
+        //state = 2;
+    }
 	
-	// Update is called once per frame
-	void Update () {
+	// Loop is called once per frame, AKA game loop
+	public void Loop () {
 		
 	}
 }
