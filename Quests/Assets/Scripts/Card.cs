320c87cd813ac8d702f33bf683c660d2d1efc991
RoyalD11 <dodge.a.schs@gmail.com>
2018-01-28 13:16:13 -0500

    Added almost full skeleton for the event class, added all the card sprites we will be using

--- Quests/Assets/Scripts/Card.cs
+++ Quests/Assets/Scripts/Card.cs
@@ -3,6 +3,8 @@
 namespace QuestOTRT
 {
 
+    //OUR CARDS NEED TO HAVE THE EXACT NAMES AS THE EVENT CLASS NAMES
+
     public abstract class Card
     {
         protected string name;
--- Quests/Assets/Scripts/Event.cs
+++ Quests/Assets/Scripts/Event.cs
@@ -8,18 +8,89 @@ namespace QuestOTRT
         //Constructor
         public Event(string name) : base(name){}
 
-        //A switch statement which calls the card that was drawn
-        public void play()
+        //A switch statement which calls the card that was drawn, THESE NAMES NEED TO BE EXACTLY WHAT THE CARDS ARE CALLED
+        public void play(string name)
         {
             switch (name)
             {
                 case "Chivalrous Deed":
                     chivalrousDeed();
+                    break;
+
+                case "Court Called to Camelot":
+                    courtCalled();
+                    break;
+
+                case "King's Call to Arms":
+                    kingCall();
+                    break;
+
+                case "King's Recognition":
+                    kingRecognition();
+                    break;
+
+                case "Plague":
+                    plague();
+                    break;
+
+                case "Pox":
+                    pox();
+                    break;
+
+                case "Prosperity Throughout the Realm":
+                    prosperityTtR();
+                    break;
+
+                case "Queen's Favor":
+                    queensFavor();
+                    break;
+
+                default:
+                    break;
             }
         }
 
         //Functions for all the functionality of our events
-        public void chivalrousDeed() { }
+        public void chivalrousDeed()
+        {
+            //Player(s) with lowest rank and least amount of shields gains 3 shields
+        }
+
+        public void courtCalled()
+        {
+            //All Allies in play are discarded
+        }
+
+        public void kingCall()
+        {
+            //Highest ranked player(s) must place 1 weapon in the discard
+                //If unable to do so, 2 foe cards must be discarded
+        }
+
+        public void kingRecognition()
+        {
+            //Next player(s) to complete a Quest reicieve 2 extra shields
+        }
+
+        public void plague()
+        {
+            //Drawer loses 2 shields, if possible
+        }
+
+        public void pox()
+        {
+            //All other players lose one shield(if possible), drawer of this card is exempt
+        }
+
+        public void prosperityTtR()
+        {
+            //All players can immediately draw 2 Adventure Cards
+        }
+
+        public void queensFavor()
+        {
+            //Lowest ranked player(s) immediately recieve 2 Adventure Cards
+        }
     }
 }
 
