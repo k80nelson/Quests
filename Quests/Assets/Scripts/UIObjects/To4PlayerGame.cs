716bbcce7924d620a2de101c406c79effbd77986
jerbyz <jer.crinson@gmail.com>
2018-02-14 19:53:20 -0500

    assests linked

---
+++ Quests/Assets/Scripts/UIObjects/To4PlayerGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class To4PlayerGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnClick()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("4pGame");
+    }
+}
