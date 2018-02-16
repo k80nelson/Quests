a27a8ca631be592d91823566ff3c2fd984fa6530
RoyalD11 <dodge.a.schs@gmail.com>
2018-02-15 19:48:35 -0500

    trying to get a volume adjuster working in settings, set up the settings canvas

---
+++ Quests/Assets/Scripts/UIObjects/VolumeScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public UnityEngine.UI.Slider volumeSlider;
+    public AudioSource volumeAudio;
+    public void VolumeController()
+    {
+        volumeAudio.volume = volumeSlider.value;
+    }
+}
