9e82687da75b692a4ea2ef4611938d158343a84d
RoyalD11 <dodge.a.schs@gmail.com>
2018-02-21 16:06:09 -0500

    started working on tournements, added a potetntial update function to game, fixed the placement on the player indicators

--- Quests/Assets/Scripts/Game.cs
+++ Quests/Assets/Scripts/Game.cs
@@ -1,38 +1,47 @@
 ﻿using System;
 using UnityEngine;
 
-public class GameElement : MonoBehaviour
+namespace QuestOTRT
 {
-    public Game gameState { get { return GameObject.FindObjectOfType<Game>(); } }
-}
-
-public class Game : MonoBehaviour
-{
-    public int numPlayers;
-    public int numControlled;
-    public GameObject[] Players;
-    public DeckController Deck;
-
-    void Start()
+    public class GameElement : MonoBehaviour
     {
-        Deck = GameObject.FindWithTag("Deck").GetComponent<DeckController>();
-        initPlayers();
+        public Game gameState { get { return GameObject.FindObjectOfType<Game>(); } }
     }
 
-    void Update()
+    public class Game : MonoBehaviour
     {
-        
-    }
+        public int numPlayers;
+        public int numControlled;
+        public GameObject[] Players;
+        public DeckController Deck;
 
-    void initPlayers()
-    {
-        PlayerController ctrl;
-        foreach (GameObject player in Players)
+        void Start()
         {
-            ctrl = player.GetComponent<PlayerController>();
-            ctrl.addCards(Deck.DrawAdventureCards(12));
+            Deck = GameObject.FindWithTag("Deck").GetComponent<DeckController>();
+            initPlayers();
         }
 
-        Debug.Log(Deck.numAdvCards());
+        void Update()
+        {
+
+        }
+
+        void initPlayers()
+        {
+            PlayerController ctrl;
+            foreach (GameObject player in Players)
+            {
+                ctrl = player.GetComponent<PlayerController>();
+                ctrl.addCards(Deck.DrawAdventureCards(12));
+            }
+
+            Debug.Log(Deck.numAdvCards());
+        }
+
+        //Late Update runs whatever is inside it at the end of the loop, it updates last, so anythign that needs to be done at the end should be done here
+        void LateUpdate()
+        {
+
+        }
     }
 }
--- Quests/Assets/Scripts/Model/Tournament.cs
+++ Quests/Assets/Scripts/Model/Tournament.cs
@@ -5,6 +5,8 @@ namespace QuestOTRT
     public class Tournament : StoryCard
     {
         private int shields;
+        public int numPlayers;
+
         public int Shields
         {
             get
@@ -18,5 +20,49 @@ namespace QuestOTRT
         {
             this.shields = shields;
         }
+
+
+        //Game logic for tournements
+        /*
+         * 
+         * input: reference to the players joining the tourney
+         * returns: nothing
+         *  
+         */
+        public void playTournement(Player[] players)
+        {
+           
+            //Gets the number of players involved in the tournement
+            numPlayers = players.Length;
+
+            return;
+        }
     }
 }
+
+/* Timeline of a Tournement
+ * 
+ * card is drawn
+ * 
+ * players decide who wants to be part of the tournement
+ *      starting with person drawing card, then following turn order
+ *   
+ *   If only one person enters they win 1 shield + bonus
+ *   
+ * each player entering draws one adventure card
+ * 
+ * players decide what cards they want to play from their hand (if they want to play any cards)
+ * 
+ * All cards are shown at the same time
+ * 
+ * Person with hiest BP wins
+ * 
+ *      If there is a tie:
+ *         Weapons are discarded
+ *         Repeat tournement play with those who tied, play more cards and reveal
+ *         if theres another tie they both win
+ *         
+ * winner recieves:
+ *      the number of shields equal to the number of players in the tournement
+ *      the number of bonus shields on the card listed as bonus 
+ */
