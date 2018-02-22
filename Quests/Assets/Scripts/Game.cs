9ebdd05ad259123219b9d61fd3df95770985fe3a
jerbyz <jer.crinson@gmail.com>
2018-02-22 16:57:25 -0500

    Added Game.cs script to 4pgame

--- Quests/Assets/Scripts/Game.cs
+++ Quests/Assets/Scripts/Game.cs
@@ -12,12 +12,13 @@ namespace QuestOTRT
     {
         public int numPlayers;
         public int numControlled;
-        public GameObject[] Players;
-        public DeckController Deck;
+        public GameObject[] players;
+        public DeckController deck;
 
         void Start()
         {
-            Deck = GameObject.FindWithTag("Deck").GetComponent<DeckController>();
+            //Deck = GameObject.FindWithTag("Deck").GetComponent<DeckController>();
+            deck = new DeckController();
             initPlayers();
         }
 
@@ -29,13 +30,13 @@ namespace QuestOTRT
         void initPlayers()
         {
             PlayerController ctrl;
-            foreach (GameObject player in Players)
+            foreach (GameObject player in players)
             {
                 ctrl = player.GetComponent<PlayerController>();
-                ctrl.addCards(Deck.DrawAdventureCards(12));
+                ctrl.addCards(deck.DrawAdventureCards(12));
             }
 
-            Debug.Log(Deck.numAdvCards());
+            Debug.Log(deck.numAdvCards());
         }
 
         //Late Update runs whatever is inside it at the end of the loop, it updates last, so anythign that needs to be done at the end should be done here
