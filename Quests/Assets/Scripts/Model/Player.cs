dd9c1eb2e328d0f6224b824bd048146fe7a26805
RoyalD11 <dodge.a.schs@gmail.com>
2018-02-24 20:00:40 -0500

    merge needed

--- Quests/Assets/Scripts/Model/Player.cs
+++ Quests/Assets/Scripts/Model/Player.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace QuestOTRT
 {
-    public class Player
+    public class Player : MonoBehaviour
     {
         private int shields;
         private int bp;
