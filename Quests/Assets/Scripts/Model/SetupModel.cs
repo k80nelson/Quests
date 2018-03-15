18ae24a1bcd04076645497cf6da4fe9378e49cbf
RoyalD11 <dodge.a.schs@gmail.com>
2018-03-15 18:50:44 -0400

    Added model classes that will be used for quests
    Stage Model is a list of adventure cards that will represent the cards that each player plays for each tage of a quest. Can use the same base for the sponsor
    
    Setup Model is a list of stages, and will hold all the cards played for each seperate stage. Again sponsor works similarly to how the players do.
    
    Stage = list of adventure cards
    Setup = list of stages
    
    so all together
    setup = list of list of cards

---
+++ Quests/Assets/Scripts/Model/SetupModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace QuestOTRT
+{
+    public class SetupModel
+    {
+
+        private List<StageModel> stageSetup;
+
+        //Constructor to create the setup for the stage
+        public SetupModel()
+        {
+            stageSetup = new List<StageModel>();
+        }
+
+        //Returns the number of players involved in that quest
+        //if it is 1, then there are no players and only the sponsor in the quest, meaning quest is over.
+        public int Count
+        {
+            get
+            {
+                return stageSetup.Count;
+            }
+        }
+
+        //Ability to add one players cards to the list, used for the players on the quest
+        public void Add(StageModel stage)
+        {
+            if (stageSetup == null) stageSetup = new List<StageModel>();
+            stageSetup.Add(stage);
+        }
+
+        //Ability to add a list of stages to the list, used for the sponsor for the quest
+        public void add(List<StageModel> stages)
+        {
+            if (stageSetup == null) stageSetup = new List<StageModel>();
+            this.stageSetup.AddRange(stages);
+        }
+    }
+}
---
+++ Quests/Assets/Scripts/Model/StageModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace QuestOTRT
+{
+    public class StageModel
+    {
+
+        private List<AdventureCard> cardsPlayed;
+
+        public StageModel()
+        {
+            cardsPlayed = new List<AdventureCard>();
+        }
+
+        //Returns the number of cards for that stage
+        public int Count
+        {
+            get
+            {
+                return cardsPlayed.Count;
+            }
+        }
+
+        public void Add(AdventureCard card)
+        {
+            if (cardsPlayed == null) cardsPlayed = new List<AdventureCard>();
+            cardsPlayed.Add(card);
+        }
+
+        public void add(List<AdventureCard> cards)
+        {
+            if (cards == null) cards = new List<AdventureCard>();
+            this.cardsPlayed.AddRange(cards);
+        }
+    }
+}
