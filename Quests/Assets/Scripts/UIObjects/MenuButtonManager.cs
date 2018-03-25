347d79032ff0afe85708ce2795216e7714103c39
RoyalD11 <dodge.a.schs@gmail.com>
2018-03-25 08:54:38 -0400

    Got the menu displaying the proper player numbers

--- Quests/Assets/Scripts/UIObjects/MenuButtonManager.cs
+++ Quests/Assets/Scripts/UIObjects/MenuButtonManager.cs
@@ -26,10 +26,12 @@ public class MenuButtonManager : MonoBehaviour {
         switch (numActive)
         {
             case 3:
+                text.text = "3"; //Sets the display text in the choose player menu to 3
                 adBtn.interactable = true;
                 player4.SetActive(false);
                 break;
             case 2:
+                text.text = "2"; //Sets the display text in the choose player menu to 2
                 rmBtn.interactable = false;
                 player3.SetActive(false);
                 break;
@@ -44,10 +46,12 @@ public class MenuButtonManager : MonoBehaviour {
         switch (numActive)
         {
             case 3:
+                text.text = "3"; //Sets the display text in the choose player menu to 3
                 player3.SetActive(true);
                 rmBtn.interactable = true;
                 break;
             case 4:
+                text.text = "4"; //Sets the display text in the choose player menu to 4
                 player4.SetActive(true);
                 adBtn.interactable = false;
                 break;
