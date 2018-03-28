3dd4b75f1dd588c5f519a6851741793d2b13d38c
RoyalD11 <dodge.a.schs@gmail.com>
2018-03-28 17:54:43 -0400

    got testvalids last case working

--- Quests/Assets/Scripts/Controllers/Sponsor.cs
+++ Quests/Assets/Scripts/Controllers/Sponsor.cs
@@ -16,6 +16,8 @@ public class Sponsor : MonoBehaviour {
     public GameObject promptBox;
     public Text promptText;
 
+    bool testFlag = false;
+
 private void OnEnable()
     {
         //storyCardTransform = new GameObject().transform;
@@ -54,23 +56,39 @@ private void OnEnable()
 
     public bool testValid(int id, Draggable d)
     {
+        testFlag = false;
+
         AdventureCard currCard = d.gameObject.GetComponent<AdventureCard>();
 
         if (currCard.type == AdventureCard.Type.AMOUR || currCard.type == AdventureCard.Type.ALLY) return false;
 
         List<AdventureCard> currStage = new List<AdventureCard>(stagesObjects[id].GetComponentsInChildren<AdventureCard>());
 
+        if (currStage.Find(i => i.type == AdventureCard.Type.TEST) != null) return false;
+
+        if ((currCard.type == AdventureCard.Type.FOE) && currStage.Find(i => i.type == AdventureCard.Type.FOE) != null) return false;
+
         if ((currCard.type == AdventureCard.Type.WEAPON) && 
             ((currStage.Find(i => i.type == AdventureCard.Type.TEST) != null) || 
             !(currStage.Find(i => i.type == AdventureCard.Type.FOE) != null) || 
             (currStage.Find(i=>i.Name == currCard.Name) != null))) return false;
 
-        if ((currCard.type == AdventureCard.Type.TEST) && (currStage.Count > 0)) return false;
+        //This nested for loop is to check if there is a test already in play
+        for (int i = 0; i < stages; i++)
+        {
+            List<AdventureCard> testStage = new List<AdventureCard>(stagesObjects[i].GetComponentsInChildren<AdventureCard>());
+            for(int j = 0; j<testStage.Count; j++)
+            {
+                if (testStage[j].type == AdventureCard.Type.TEST)
+                {
+                    testFlag = true;
+                }
+            }
+        }
 
-        if (currStage.Find(i => i.type == AdventureCard.Type.TEST) != null) return false;
+        if ((currCard.type == AdventureCard.Type.TEST) && ((currStage.Count > 0) || testFlag)) return false;
 
-        if ((currCard.type == AdventureCard.Type.FOE) && currStage.Find(i => i.type == AdventureCard.Type.FOE) != null) return false;
-
+        
         return true;
     }
 
