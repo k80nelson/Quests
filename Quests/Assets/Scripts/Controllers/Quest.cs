0605634e97312d2ffdccd38c9f49986e40a3e12b
RoyalD11 <dodge.a.schs@gmail.com>
2018-03-31 14:38:02 -0400

    Added some prompts
    Changed heiarchy for quest prompt, it was showing up below the stages
    Added prompts to say who failed stages
    WARNING
    somehow we have a divide by 0 that can happen from line 126 in Quests, possibly think all players failing triggered it

--- Quests/Assets/Scripts/Controllers/Quest.cs
+++ Quests/Assets/Scripts/Controllers/Quest.cs
@@ -264,6 +264,7 @@ public class Quest : GameElement
             else
             {
                 Debug.Log("[Quest.cs:findPassingPlayers] player " + (player.index + 1) + " failed stage " + (currStageId + 1));
+                promptUser("Player " + (player.index + 1) + " has failed stage " + (currStageId + 1));
                 players.Remove(player);
                 playerIds.Remove(player.index);
                 numPlayers -= 1;
@@ -309,6 +310,7 @@ public class Quest : GameElement
     public void endFail()
     {
         Debug.Log("[Quest.cs:endFail] All players have failed the Quest");
+        promptUser("All players have failed the Quest");
         end();
     }
 
