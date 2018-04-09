5b2a84bb28a88430714d96251e1c6740120942fd
Katie Nelson <KatieNelson@cmail.carleton.ca>
2018-04-09 00:49:22 -0400

    Fixing Deck Controller

--- Quests/Assets/Scripts/Local/DeckController.cs
+++ Quests/Assets/Scripts/Local/DeckController.cs
@@ -1,11 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
-using UnityEngine.EventSystems;
 
 
-public class DeckController : NetworkBehaviour, IDropHandler//, IPointerEnterHandler, IPointerExitHandler
+public class DeckController : MonoBehaviour
 {
 
     #region singleton
@@ -28,39 +26,4 @@ public class DeckController : NetworkBehaviour, IDropHandler//, IPointerEnterHan
     {
         return advDeck.drawMany(num);
     }
-
-    //when player drops a card over the discard area
-    public void OnDrop(PointerEventData data)
-    {
-        Debug.Log("In the on drop function to remove a card");
-        Draggable d = data.pointerDrag.GetComponent<Draggable>();
-        Debug.Log(d);
-        if (d != null)
-        {
-            Debug.Log("d was not null");
-            Debug.Log("The name of the to discard is " + d.gameObject.name);
-            toRemove = d.gameObject;
-            //confirm.SetActive(true);
-            tmpCard = Instantiate(toRemove, discardTransform);
-            tmpCard.transform.localPosition = new Vector2(0, 0);
-            discardCard();
-        }
-    }
-
-    public void discardCard()
-    {
-        //deck .discard(toRemove);
-        //game.players[game.activePlayer].GetComponent<PlayerController>().discardCard(toRemove);
-    }
-
-
-    //discarding cards hopefully
-    public void discard()
-    {
-        //if (!isLocalPlayer) return;
-
-        Debug.Log("[StoryDeckController.cs::discard] Story card discarded");
-        //Destroy(CardTransform.transform.GetChild(0).gameObject);
-    }
-
 }
