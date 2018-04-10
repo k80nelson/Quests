301cdcd31180ae8e30286506cc8e449fee681c94
Katie Nelson <KatieNelson@cmail.carleton.ca>
2018-04-10 03:47:02 -0400

    added Player Model functions

--- Quests/Assets/Game/Scripts/NetPlayerModel.cs
+++ Quests/Assets/Game/Scripts/NetPlayerModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public enum Rank { Squire, Knight, Champion };
+public enum Rank { Squire, Knight, Champion, RoundTable };
 public class NetPlayerModel : NetworkBehaviour {
 
     protected PlayerView _view;
@@ -23,6 +23,7 @@ public class NetPlayerModel : NetworkBehaviour {
     public int bp = 5;
 
     Hand _hand;
+    List<AdventureCard> _allies;
 
     private void Awake()
     {
@@ -35,6 +36,63 @@ public class NetPlayerModel : NetworkBehaviour {
         if (_hand == null) _hand = ScriptableObject.CreateInstance<Hand>();
         _hand.Add(card);
         cards += 1;
+    }
+
+    [Server]
+    public void AddAlly(AdventureCard card)
+    {
+        if (card == null) return;
+        if (_allies == null) _allies = new List<AdventureCard>();
+        if (card.type == AdventureCardType.ALLY) _allies.Add(card);
+    }
+
+    [Server]
+    public void AddAllies(List<AdventureCard> cards)
+    {
+        if (cards == null) return;
+        foreach (AdventureCard card in cards)
+        {
+            AddAlly(card);
+        }
+    }
+
+    [Server]
+    public List<AdventureCard> removeAllies()
+    {
+        List<AdventureCard> ret = new List<AdventureCard>(_allies);
+        _allies.Clear();
+        return ret;
+    }
+
+    [Server]
+    public void removeShields(int num)
+    {
+        this.shields -= num;
+        if (num < 0)
+            shields = 0;
+    }
+
+    [Server]
+    public bool hasTest()
+    {
+        return _hand.containsTest();
+    }
+
+    [Server]
+    public bool enoughFoes(int num)
+    {
+        return _hand.containsFoes(num);
+    }
+
+    [Server]
+    public int calculateAllyBP()
+    {
+        int total = 0;
+        foreach (AdventureCard ally in _allies)
+        {
+            total += ally.getBP();
+        }
+        return total;
     }
 
     [Server]
@@ -53,11 +111,81 @@ public class NetPlayerModel : NetworkBehaviour {
         bp = 5;
     }
 
+    bool canUpgrade(int additionalShields)
+    {
+        bool upgrade = false;
+        if (rankInt == 0 && ((shields + additionalShields) >= 5))
+        {
+            upgrade = true;
+        }
+        else if (rankInt == 1 && ((shields + additionalShields) >= 7))
+        {
+            upgrade = true;
+        }
+        else if (rankInt == 2 && ((shields + additionalShields) >= 10))
+        {
+            upgrade = true;
+        }
+        return upgrade;
+    }
+
+    [Server]
+    public bool rankUp()
+    {
+        switch (this.rankInt)
+        {
+            case 0:
+                if (shields >= 5)
+                {
+                    shields -= 5;
+                    rankInt++;
+                    bp += 5;
+                    return true;
+                }
+                return false;
+            case 1:
+                if (shields >= 7)
+                {
+                    shields -= 7;
+                    rankInt++;
+                    bp += 5;
+                    return true;
+                }
+                return false;
+            case 2:
+                if (shields >= 10)
+                {
+                    shields -= 10;
+                    rankInt++;
+                    bp += 5;
+                    return true;
+                }
+                return false;
+            default:
+                throw new System.Exception("Trying to rank up past the end game");
+        }
+    }
+
+    [Server]
+    public void addShields(int num)
+    {
+        shields += num;
+        if (canUpgrade(0))
+        {
+            rankUp();
+            PromptHandler.instance.SendPromptToAll("Rank Up!", name + " has ranked up to " + ((Rank)rankInt).ToString() +".");
+        }
+    } 
+
     // -- SyncVar hooks
     void OnRankChanged(int newVal)
     {
         rankInt = newVal;
         _view.updateRankText(newVal);
+        if (newVal == 3)
+        {
+            // GAME IS OVER
+        }
     }
 
     void OnShieldsChanged(int newVal)
