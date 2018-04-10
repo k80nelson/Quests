04c3915a942f6fd5354da9d4e12a04bab32373a7
Bert <bertholindestinvil@gmail.com>
2018-04-09 20:50:13 -0400

    updated deck controller to have method for story deck and added to game scene, Also added helper functions  for start and end turn (no functionality)

---
+++ Quests/Assets/Scripts/Networked/Game/HelperFunctions.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HelperFunctions : NetworkBehaviour {
+
+    public void startTurn()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        
+        //check players hand to make sure the have 12 or less cards
+
+            //if more than 12 
+                //print error message
+            //if 12 or less
+                //draw the story card from story deck
+                //place the story card on the screen
+                //active/play the story card that was drawn
+
+        //change the text of the button to end turn
+        
+    }
+
+    public void endTurn()
+    {
+        //remove story card from screen
+        //discard story card
+
+        //pass player turn to next player
+        //notify other players that the player is done
+        //notify the new player that its their turn
+        
+    }
+
+}
